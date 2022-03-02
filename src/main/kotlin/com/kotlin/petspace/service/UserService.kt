ba81a4e95577b8692cf6664ca4b91a160ba96ee0
Gustavo Sosa <gustavososa92@gamil.com>
2022-03-01 23:10:59 -0300

    add user service scaffolding

---
+++ src/main/kotlin/com/kotlin/petspace/service/UserService.kt
@@ -0,0 +1,30 @@
+package com.kotlin.petspace.service
+
+import com.kotlin.petspace.dto.UserLoginRequestDTO
+import com.kotlin.petspace.dto.UserLoginResponseDTO
+import com.kotlin.petspace.dto.UserRequestDTO
+import com.kotlin.petspace.dto.UserResponseDTO
+import org.springframework.stereotype.Service
+
+@Service
+class UserService {
+    fun loginUser(userLogin: UserLoginRequestDTO): UserLoginResponseDTO {
+        TODO("Not yet implemented")
+    }
+
+    fun registerUser(newUser: UserRequestDTO) {
+        TODO("Not yet implemented")
+    }
+
+    fun updateUser(userId: Long, updatedUser: UserRequestDTO) {
+        TODO("Not yet implemented")
+    }
+
+    fun searchUserById(userId: Long) : UserResponseDTO {
+        TODO("Not yet implemented")
+    }
+
+    fun deleteUserById(userId: Long) {
+        TODO("Not yet implemented")
+    }
+}
