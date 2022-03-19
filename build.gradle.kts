86548395472c09ade799342f21634128430c29be
Gustavo Sosa <gustavososa92@gamil.com>
2022-03-18 21:50:26 -0300

    add workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     kotlin("jvm") version "1.6.10"
     kotlin("plugin.spring") version "1.6.10"
     kotlin("plugin.jpa") version "1.6.10"
+    jacoco
 }
 
 group = "com.kotlin"
@@ -22,22 +23,27 @@ repositories {
     mavenCentral()
 }
 
+val kotestVersion = "5.1.0"
+
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa:2.6.3")
-    implementation("org.springframework.boot:spring-boot-starter-web:2.6.3")
+    implementation(kotlin("stdlib"))
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa:2.6.4")
+    implementation("org.springframework.boot:spring-boot-starter-web:2.6.4")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("org.springframework:spring-jdbc:5.3.15")
+    implementation("org.springframework:spring-jdbc:5.3.16")
     implementation("org.hibernate:hibernate-core:5.6.5.Final")
     implementation("org.jetbrains.kotlin:kotlin-maven-noarg")
     implementation("org.springdoc:springdoc-openapi-ui:1.6.6")
     implementation("org.springframework.boot:spring-boot-starter-validation:2.6.4")
     implementation("org.modelmapper:modelmapper:3.0.0")
-    developmentOnly("org.springframework.boot:spring-boot-devtools:2.6.3")
+    developmentOnly("org.springframework.boot:spring-boot-devtools:2.6.4")
     runtimeOnly("com.h2database:h2:1.4.198")
-    runtimeOnly("mysql:mysql-connector-java:8.0.25")
-    testImplementation("org.springframework.boot:spring-boot-starter-test:2.6.3")
+    runtimeOnly("mysql:mysql-connector-java:8.0.28")
+    testImplementation("org.springframework.boot:spring-boot-starter-test:2.6.4")
+    testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
+    testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
 }
 
 tasks.withType<KotlinCompile> {
@@ -49,4 +55,30 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+tasks.test {
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+}
+
+jacoco {
+    toolVersion = "0.8.7"
+}
+
+tasks.jacocoTestReport {
+    reports {
+        xml.isEnabled = true
+        csv.isEnabled = false
+        html.isEnabled = false
+    }
+}
+
+tasks.register("runOnGitHub") {
+    dependsOn("jacocoTestReport")
+    group = "custom"
+    description = "$ ./gradlew runOnGitHub # runs on GitHub Action"
 }
